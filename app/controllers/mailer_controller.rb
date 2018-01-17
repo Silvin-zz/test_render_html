ce73bc2f3797fdea17ee2075183f5d6783920f79
Silvin <bravocado@gmail.com>
2018-01-17 00:14:09 -0600

    Starting with render html

---
+++ app/controllers/mailer_controller.rb
@@ -0,0 +1,26 @@
+class MailerController < ActionController::API
+
+    def show
+        user = {}
+        user["name"] = "Pedrito"
+        user["products"] = [
+            {
+                "name"  => "Producto 1",
+                "cost"  => "$1200.00",
+                "tax"   => "120.00",
+                "total" => "1320.00"
+            },
+            {
+                "name"  => "Producto 2",
+                "cost"  => "$1000.00",
+                "tax"   => "100.00",
+                "total" => "1100.00"
+            }
+        ]
+        
+        data = ApplicationController.render ('mailer/data'),assigns: { user: user }
+        
+        render json: {"uno": data}, status: 200
+        
+    end
+end
